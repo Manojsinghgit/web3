f74e879063ca0a08fb676fd47db7939aa0170725
piyushbora-frontend <piyush.b@payairo.com>
2026-02-06 01:06:02 +0530

    description changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "TopupGo",
-  description: "Web3Auth NextJS Quick Start",
+  description: "TopupGo Portal",
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
